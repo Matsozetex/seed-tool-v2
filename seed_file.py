01fe6c6cc28899e68dbd3fb33b271ec9bcc752e5
Matsozetex <mattp32000@hotmail.com>
2023-12-14 09:40:49 +1100

    Update: Implemented setters on seed_file class

--- seed_file.py
+++ seed_file.py
@@ -3,22 +3,26 @@ from const import SEED_GAME_SETTINGS
 
 @dataclass
 class SeedIniFile:
-    ini_type: str
     ini_tag: str =""
-    ini_dx: bool = ""
+    ini_dx12: bool = ""
     ini_content: str = ""
 
-    def __init__(self,  ini_dx: bool, ini_type: str, ini_tag: str):
-        self.ini_type = ini_type
-        self.ini_dx = ini_dx
+    def __init__(self,  ini_dx12: bool, ini_tag: str):
+        self.ini_dx12 = ini_dx12
         self.ini_tag = ini_tag
         self.ini_content = ''
 
-    def set_seed_content(self) -> None:
-        ini_string = ''
-        if(self.ini_type == "SEED_MODE"):
-            ini_string = SEED_GAME_SETTINGS
-            ini_string = ini_string.replace("{tag}", self.ini_tag).replace("{dx}", str(self.ini_dx) )
-            self.ini_content = ini_string
+    def set_content(self) -> None:
+        ini_string = SEED_GAME_SETTINGS
+        ini_string = ini_string.replace("{tag}", self.ini_tag).replace("{dx}", str(self.ini_dx12) )
+        self.ini_content = ini_string
       
+    def set_tag(self, new_tag: str) -> None:
+        if(len(new_tag) <= 7):
+            self.ini_tag = new_tag 
+        
 
+    def set_dx(self, new_dx: bool) -> None:
+        self.ini_dx12 = new_dx
+    
+    
