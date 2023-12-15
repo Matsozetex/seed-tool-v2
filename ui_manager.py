c86730b20939fa6c9135a661a8fd277b27533e61
Matsozetex <mattp32000@hotmail.com>
2023-12-15 22:33:54 +1100

    Fix: Padding for UI

--- ui_manager.py
+++ ui_manager.py
@@ -13,15 +13,16 @@ class App(customtkinter.CTk):
         self.frame_action = customtkinter.CTkFrame(self, 150, 450, corner_radius=20, bg_color="yellow")
         
         # Settings elements
-        self.label_settings_title = customtkinter.CTkLabel(self.frame_settings, text="Seed Tool Config")
-        self.label_settings_path = customtkinter.CTkLabel(self.frame_settings, text="Launcher Path")
-        self.text_settings_path = customtkinter.CTkTextbox(self.frame_settings)
-        self.label_settings_options = customtkinter.CTkLabel(self.frame_settings, text="Ini Options")
-        self.label_settings_dx = customtkinter.CTkLabel(self.frame_settings, text="DX Version")
+        self.label_settings_title = customtkinter.CTkLabel(self.frame_settings, text="Seed Tool Config", width=150)
+        self.label_settings_path = customtkinter.CTkLabel(self.frame_settings, text="Launcher Path",width=150)
+        
+        self.text_settings_path = customtkinter.CTkTextbox(self.frame_settings, width=150, height=150)
+        self.label_settings_options = customtkinter.CTkLabel(self.frame_settings, text="Ini Options", width=150)
+        self.label_settings_dx = customtkinter.CTkLabel(self.frame_settings, text="DX Version", width=150)
         self.varcheck_settings_dx12 = customtkinter.StringVar(value="off")
-        self.radio_settings_dx12 = customtkinter.CTkCheckBox(self.frame_settings, variable=self.varcheck_settings_dx12, onvalue=1, offvalue=0)
-        self.label_settings_tag = customtkinter.CTkLabel(self.frame_settings, text="Clan Tag")
-        self.text_settings_tag=  customtkinter.CTkTextbox(self.frame_settings)
+        self.radio_settings_dx12 = customtkinter.CTkCheckBox(self.frame_settings, variable=self.varcheck_settings_dx12, onvalue=1, offvalue=0, width=150, text="Enable DX12")
+        self.label_settings_tag = customtkinter.CTkLabel(self.frame_settings, text="Clan Tag", width=150)
+        self.text_settings_tag=  customtkinter.CTkTextbox(self.frame_settings, width=150, height=30)
         
         # # File elements
         # self.label_file_title
@@ -40,14 +41,14 @@ class App(customtkinter.CTk):
         self.frame_action.grid(column=2, row=0)
     
     def grid_settings_elements(self):
-        self.label_settings_title.grid(row=0, column = 0,)
-        self.label_settings_path.grid(row=1, column = 0,)
-        self.text_settings_path.grid(row=2, column = 0,)
-        self.label_settings_options.grid(row=3, column = 0,)
-        self.label_settings_dx.grid(row=4, column = 0,)
-        self.radio_settings_dx12.grid(row=5, column = 0,)
-        self.label_settings_tag.grid(row=7, column = 0,)
-        self.text_settings_tag.grid(row=8, column = 0,)
+        self.label_settings_title.grid(row=0, column = 0, padx=2, pady=2)
+        self.label_settings_path.grid(row=1, column = 0, padx=2, pady=2)
+        self.text_settings_path.grid(row=2, column = 0, padx=2, pady=2)
+        self.label_settings_options.grid(row=3, column = 0, padx=2, pady=2)
+        self.label_settings_dx.grid(row=4, column = 0, padx=2, pady=2)
+        self.radio_settings_dx12.grid(row=5, column = 0, padx=2, pady=2)
+        self.label_settings_tag.grid(row=7, column = 0, padx=2, pady=2)
+        self.text_settings_tag.grid(row=8, column = 0, padx=2, pady=2)
         
     
     def pack_file_elements(self):
